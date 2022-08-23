cbf1fad6adab46698a8c5fbc4c8a412a05234dac
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-08-22 19:13:27 -0700

    fix address payable

--- Blockchain/Solidity/Solidity.sol
+++ Blockchain/Solidity/Solidity.sol
@@ -28,19 +28,21 @@ contract MyContract is Ownable {
 pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract Destructible is Ownable {
-    address payable payableOwner = payable(owner());
+    
     constructor() payable {
     }
     /**
      * @dev Transfers the current balance to the owner and terminates the contract.
      */
     function destroy() public onlyOwner {
+        address payable payableOwner = payable(owner());
         selfdestruct(payableOwner);
     }
     function destroyAndSend(address payable _recipient) public onlyOwner {
         selfdestruct(_recipient);
     }
 }
+
 
 
 // Make address payable
