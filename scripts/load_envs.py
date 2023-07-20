0b4c28c6309dafa0fa6870d2c8677ca65179ee54
Danny Blessing <drblessing@gmail.com>
2023-07-20 15:48:26 -0700

    load envs

--- scripts/load_envs.py
+++ scripts/load_envs.py
@@ -1,7 +1,8 @@
-def load_envs(*names) -> list:
-    import os
-    import dotenv
+import dotenv
+import os
 
+
+def load_envs(*names) -> list[str]:
     dotenv.load_dotenv()
 
     if len(names) == 1 and isinstance(names[0], list):
