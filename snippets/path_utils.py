2a81dd2cc93bdf01af8d5e9de4c050d112a05057
Danny Blessing <drblessing@gmail.com>
2024-01-27 18:17:55 -0800

    path_utils

--- snippets/path_utils.py
+++ snippets/path_utils.py
@@ -1,4 +1,4 @@
-"""This module contains functions to handle paths.
+"""This module contains functions to handle paths. 
 
 Functions: 
     get_module_path: Get the path of the current module.
@@ -10,21 +10,35 @@ Functions:
 from pathlib import Path
 
 
-def get_module_path() -> Path:
-    """Get the path of the current module."""
+def get_module_path(file: str) -> Path:
+    """Get the path of the current module.
+    Pass __file__ as argument."""
 
-    return Path(__file__)
+    return Path(file)
 
 
-def get_log_dir() -> Path:
+def get_log_dir(file: str) -> Path:
     """Gets the path of a "logs" directory in the same
     directory as the current module, i.e. as a brother directory.
     """
 
-    return get_module_path().parent / "logs"
+    return get_module_path(file).parent / "logs"
 
 
 def make_log_dir(log_dir: Path) -> None:
     """Create the brother log directory if it doesn't exist."""
 
     log_dir.mkdir(parents=True, exist_ok=True)
+
+
+# Example
+if __name__ == "__main__":
+    # Create the log directory here
+    log_dir = get_log_dir(__file__)
+    # Check that the directory exists
+    make_log_dir(log_dir)
+    assert log_dir.exists()
+    # Delete the log directory
+    log_dir.rmdir()
+    # Check that the directory doesn't exist
+    assert not log_dir.exists()
---
+++ snippets/tests/test_path_utils.py
@@ -0,0 +1,41 @@
+import pytest
+from snippets.path_utils import get_module_path, get_log_dir, make_log_dir
+from pathlib import Path
+
+
+def test_get_module_path():
+    """Test get_module_path."""
+
+    # Get the path of this test module
+    module_path = get_module_path(__file__)
+
+    # Check that the past is correct up to Github repo name
+    correct_module_path = Path("notes", "snippets", "tests", "test_path_utils.py")
+    assert str(module_path).endswith(str(correct_module_path))
+
+
+def test_get_log_dir():
+    """Test get_log_dir."""
+
+    # Get the path of the log directory
+    log_dir = get_log_dir(__file__)
+
+    # Check that the past is correct up to Github repo name
+    correct_log_dir = Path("notes", "snippets", "tests", "logs")
+    assert str(log_dir).endswith(str(correct_log_dir))
+
+
+def test_make_log_dir():
+    """Test make_log_dir."""
+
+    # Get the path of the log directory
+    log_dir = get_log_dir(__file__)
+
+    # Create the log directory
+    make_log_dir(log_dir)
+
+    # Check that the directory exists
+    assert log_dir.exists()
+
+    # Delete the log directory
+    log_dir.rmdir()
