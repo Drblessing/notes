f78ae811d50e05a837d26f271d7a56447725da96
Danny Blessing <drblessing@gmail.com>
2024-01-27 20:39:48 -0800

    directory.sh

--- bin/directory.sh
+++ bin/directory.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-du -sh * | sort -h
+# Print the size of all visible and hidden subdirectories and files in the current directory, sorted by size.
+# Fix no .* matches error by making a temprorary hidden file.
+temp_file=$(mktemp .hiddenXXXXXX)
+du -sh .[^.]* */ | sort -h
+rm "$temp_file"
