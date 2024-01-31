fa15a49fe3b32e0f40d874bf29a307c949255a12
Danny Blessing <drblessing@gmail.com>
2024-01-30 21:27:29 -0800

    call_stack

---
+++ snippets/call_stack.py
@@ -0,0 +1,21 @@
+def print_message(message):
+    print(message)
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def calculate_factorial(number):
+    print_message(f"Calculating the factorial of {number}")
+    result = factorial(number)
+    print_message(f"The factorial of {number} is {result}")
+    return result
+
+def main():
+    number = 5
+    calculate_factorial(number)
+
+if __name__ == "__main__":
+    main()
