d0f7a2d3340698750f81208a4868959746b8c464
Danny Blessing <drblessing@gmail.com>
2024-02-01 13:27:05 -0800

    restructuring

--- projects/domains.md
+++
@@ -1,7 +0,0 @@
-# Domains
-
-## Domains I own
-
-dbless.dev, google domains <br>
-dblessing.dev, google domains <br>
-hackjack.money, google domains
--- scripts/media/flac_to_mp3.sh
+++
@@ -1,5 +0,0 @@
-# This script converts FLAC files to MP3 format
-for file in *.flac; do
-    # Convert each FLAC file to MP3 format
-    ffmpeg -i "$file" -ab 256k -id3v2_version 3 "${file%.flac}.mp3"
-done
