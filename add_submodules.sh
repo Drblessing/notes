612f3ab3e5f06cf7ff83105d0acb87f618b1ed57
Daniel Blessing <drblessing@gmail.com>
2025-04-10 12:00:46 -0400

    pushing another fix

--- add_submodules.sh
+++
@@ -1,8 +0,0 @@
-# Create directory for submodules
-mkdir subrepositories
-
-# Add each repository as a submodule
-git submodule add https://github.com/expo/expo.git subrepositories/expo
-git submodule add https://github.com/vercel/next.js.git subrepositories/next.js
-git submodule add https://github.com/cloudflare/cloudflare-docs.git subrepositories/cloudflare-docs
-git submodule add https://github.com/reactjs/react.dev.git subrepositories/react.dev
