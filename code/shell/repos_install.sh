52010f506cdb060b023d3a1a4b11f252f53d1577
Daniel Blessing <drblessing@gmail.com>
2025-04-10 16:00:06 -0400

    update

--- code/shell/repos_install.sh
+++ code/shell/repos_install.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
+
 # Install my favorite personal repos into ~/Github
+
 
 # Create directory if it doesn't exist
 mkdir -p ~/Github
 
-# Change to the Github directory
-cd ~/Github
-
 # Clone the repositories
-gh repo clone Drblessing/blockchain-development
-gh repo clone Drblessing/react-learn
-gh repo clone Drblessing/learn-expo
-gh repo clone Drblessing/private-workbench
+gh repo clone Drblessing/blockchain-development ~/Github
+gh repo clone Drblessing/react-learn ~/Github
+gh repo clone Drblessing/learn-expo ~/Github
+gh repo clone Drblessing/private-workbench ~/Github
