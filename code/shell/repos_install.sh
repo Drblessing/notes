39c41189d370bfab853bada77bcd5ca558625c31
Daniel Blessing <drblessing@gmail.com>
2025-04-10 15:41:10 -0400

    Apparently works-for-me is a crappy excuse.

---
+++ code/shell/repos_install.sh
@@ -0,0 +1,13 @@
+# Install my favorite personal repos into ~/Github
+
+# Create directory if it doesn't exist
+mkdir -p ~/Github
+
+# Change to the Github directory
+cd ~/Github
+
+# Clone the repositories
+gh repo clone Drblessing/blockchain-development
+gh repo clone Drblessing/react-learn
+gh repo clone Drblessing/learn-expo
+gh repo clone Drblessing/private-workbench
