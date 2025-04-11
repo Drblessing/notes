047bbf271bc023e8d7c607c58be6a156adabcbca
Daniel Blessing <drblessing@gmail.com>
2025-04-11 16:28:53 -0400

    PEBKAC

--- code/shell/submodules_install.sh
+++ code/shell/submodules_install.sh
@@ -2,3 +2,8 @@ git submodule add https://github.com/reactjs/react.dev.git subrepos/react.dev
 git submodule add https://github.com/expo/expo.git subrepos/expo
 git submodule add https://github.com/vercel/next.js.git subrepos/next.js
 git submodule add https://github.com/cloudflare/cloudflare-docs.git subrepos/cloudflare-docs
+git config -f .gitmodules submodule.subrepos/react.dev.ignore all
+git config -f .gitmodules submodule.subrepos/expo.ignore all
+git config -f .gitmodules submodule.subrepos/next.js.ignore all
+git config -f .gitmodules submodule.subrepos/cloudflare-docs.ignore all
+# gh repo clone notes -- -recurse-submodules    
