ad4a4fa58ef016929657719f0f939ba01d586bbd
Daniel Blessing <drblessing@gmail.com>
2025-04-16 18:37:25 -0400

    bad things happen when you forget about that one little change you made ages ago

--- code/shell/submodules_install.sh
+++ code/shell/submodules_install.sh
@@ -2,10 +2,11 @@ git submodule add --depth 1 https://github.com/reactjs/react.dev.git subrepos/re
 git submodule add --depth 1 https://github.com/expo/expo.git subrepos/expo
 git submodule add --depth 1 https://github.com/vercel/next.js.git subrepos/next.js
 git submodule add --depth 1 https://github.com/cloudflare/cloudflare-docs.git subrepos/cloudflare-docs
-# Three.js
-git submodule add 
+# Three.js for cool 3D stuff
+git submodule add --depth 1 https://github.com/mrdoob/three.js.git subrepos/three.js
 git config -f .gitmodules submodule.subrepos/react.dev.ignore all
 git config -f .gitmodules submodule.subrepos/expo.ignore all
 git config -f .gitmodules submodule.subrepos/next.js.ignore all
 git config -f .gitmodules submodule.subrepos/cloudflare-docs.ignore all
+git config -f .gitmodules submodule.subrepos/three.js.ignore all
 # git submodule update --init --recursive --depth 1
