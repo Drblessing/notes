1f69a4440dd7fa08de8eb7fd366393323af57be9
Daniel Blessing <drblessing@gmail.com>
2025-04-16 18:32:56 -0400

    TODO: Tell someone to implement this

--- code/shell/submodules_install.sh
+++ code/shell/submodules_install.sh
@@ -2,6 +2,8 @@ git submodule add --depth 1 https://github.com/reactjs/react.dev.git subrepos/re
 git submodule add --depth 1 https://github.com/expo/expo.git subrepos/expo
 git submodule add --depth 1 https://github.com/vercel/next.js.git subrepos/next.js
 git submodule add --depth 1 https://github.com/cloudflare/cloudflare-docs.git subrepos/cloudflare-docs
+# Three.js
+git submodule add 
 git config -f .gitmodules submodule.subrepos/react.dev.ignore all
 git config -f .gitmodules submodule.subrepos/expo.ignore all
 git config -f .gitmodules submodule.subrepos/next.js.ignore all
