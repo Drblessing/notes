fbf3b1bd7a586da277e901e3b263e2e4ad23ff75
Daniel Blessing <drblessing@gmail.com>
2025-06-25 00:14:41 -0400

    Never before had a small typo like this one caused so much damage.

--- linux/scripts/ufw_install.sh
+++ linux/scripts/ufw_install.sh
@@ -11,6 +11,10 @@ sudo ufw allow 8443
 sudo ufw allow 3000
 sudo ufw allow 5000
 sudo ufw allow 8000
+sudo ufw allow 32400/tcp comment "Allow Plex Media Server"
+sudo ufw allow 32400/udp comment "Allow Plex Media Server"
+sudo ufw allow 1900/udp comment "Allow Plex discovery"
+sudo ufw allow 5353/udp comment "Allow mDNS for Plex"
 sudo ufw limit ssh
 sudo ufw disable
 sudo ufw enable
