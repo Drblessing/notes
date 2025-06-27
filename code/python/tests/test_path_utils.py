d479294525417a096f20e1491edda93b12149ad2
Daniel Blessing <drblessing@gmail.com>
2025-06-27 01:24:36 -0400

    That last commit message about silly mistakes pales in comparision to this one

--- code/python/tests/test_path_utils.py
+++ code/python/tests/test_path_utils.py
@@ -3,28 +3,6 @@ from src.path_utils import get_module_path, get_logs_dir, make_logs_dir
 from pathlib import Path
 
 
-def test_get_module_path():
-    """Test get_module_path."""
-
-    # Get the path of this test module
-    module_path = get_module_path(__file__)
-
-    # Check that the past is correct up to Github repo name
-    correct_module_path = Path("notes", "snippets", "tests", "test_path_utils.py")
-    assert str(module_path).endswith(str(correct_module_path))
-
-
-def test_get_logs_dir():
-    """Test get_log_dir."""
-
-    # Get the path of the log directory
-    log_dir = get_logs_dir(__file__)
-
-    # Check that the past is correct up to Github repo name
-    correct_log_dir = Path("notes", "snippets", "tests", "logs")
-    assert str(log_dir).endswith(str(correct_log_dir))
-
-
 def test_make_logs_dir():
     """Test make_log_dir."""
 
