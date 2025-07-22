e56c287e1777abe7e5e604146f099cf6f1ad2203
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2025-07-22 12:28:50 -0400

    update

--- linux/shell/docker_install.sh
+++ linux/shell/docker_install.sh
@@ -1,14 +1,9 @@
 # Install and fix docker for snap installation
-
 # Install if not already installed
 sudo snap install --ignore-running docker
 
 # Fix permissions for docker group
-sudo groupadd docker || true
-sudo usermod -aG docker $USER || true
+sudo snap connect docker:home
 
-# Reload group memberships
-newgrp docker || true
-
-# Verify docker installation without sudo 
+# Verify docker installation
 docker run hello-world
