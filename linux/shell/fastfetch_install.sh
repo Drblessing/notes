389af02b4e07ab933634d37e7651e16597b07d47
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2025-07-22 11:16:19 -0400

    updates

--- linux/shell/fastfetch_install.sh
+++
@@ -1,6 +0,0 @@
-# Install Fastfetch 
-echo "Installing Fastfetch..."
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
-sudo apt update
-sudo apt install -y fastfetch
-echo "✅ Fastfetch installation complete."
