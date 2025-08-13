a25762f7d550f0f9d9369860ef08bb8f5d01f546
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2025-08-12 21:20:12 -0400

    update snap install

---
+++ linux/shell/snap_install.sh
@@ -0,0 +1,4 @@
+# Install snap packages and other useful packages with different installers.
+sudo snap install docker
+curl -fsSL https://tailscale.com/install.sh | sh
+curl -fsSL https://deno.land/install.sh | sh
