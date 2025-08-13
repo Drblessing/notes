297f3d62144c35b332afddd2068e8c15b55f7b17
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2025-08-13 17:01:38 -0400

    updates

--- linux/shell/docker_install.sh
+++ linux/shell/docker_install.sh
@@ -1,9 +1,28 @@
-# Install and fix docker for snap installation
-# Install if not already installed
-sudo snap install --ignore-running docker
+# Installs docker on Ubuntu.
 
-# Fix permissions for docker group
-sudo snap connect docker:home
+# Remove old docker installations
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Verify docker installation
-docker run hello-world
+# Remove docker volumes
+rm -rf /var/lib/docker
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
