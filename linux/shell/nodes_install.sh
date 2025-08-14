0213fcc32d88f49d1b85b2168226a8eb29ab425b
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2025-08-14 11:39:53 -0400

    nodes_install

---
+++ linux/shell/nodes_install.sh
@@ -0,0 +1,8 @@
+# Install public goods infrastructure.
+
+# Ethereum node
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/coincashew/EthPillar/main/install.sh)"
+# ethpillar
+
+# Bitcoin node
+
