54c64faf5a61242e00c22e7c2b6cc4a983ca2a0c
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2025-08-14 11:17:32 -0400

    updates

--- linux/shell/docker_install.sh
+++ linux/shell/docker_install.sh
@@ -26,3 +26,7 @@ sudo docker run hello-world
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# Portainer
+# ```docker volume create portainer_data
+# docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts```
