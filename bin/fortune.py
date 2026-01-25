f69efa606ed593758cc750a53213d261e579eeea
Daniel Blessing <52829488+Drblessing@users.noreply.github.com>
2026-01-25 16:00:52 -0500

    updates

--- bin/fortune.py
+++ bin/fortune.py
@@ -1,8 +1,8 @@
-
 #!/Users/danielblessing/github/notes/bin/.benv/bin/python
 
 import random
 import pyfiglet
+
 
 def cowboy_fortune():
     fortunes = [
@@ -11,13 +11,14 @@ def cowboy_fortune():
         "The best way to get a new rope is to need an old one.",
         "If you're ridin' ahead of the herd, take a look back every now and then to make sure it's still there.",
         "When you find yourself in a hole, the first thing to do is stop diggin'.",
-        "Lettin' the cat outta the bag is a whole lot easier than puttin' it back in."
+        "Lettin' the cat outta the bag is a whole lot easier than puttin' it back in.",
     ]
 
     art = pyfiglet.figlet_format("Howdy!", font="slant")
     print(art)
     print("Your cowboy fortune for the day:")
     print(f"    \__{random.choice(fortunes)}__/")
+
 
 if __name__ == "__main__":
     cowboy_fortune()
